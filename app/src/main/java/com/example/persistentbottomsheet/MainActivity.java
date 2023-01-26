1aec39461b646b551aba6ad33c4ddaf389aa6af9
tungfpv <51527621+tungfpv@users.noreply.github.com>
2023-01-26 20:42:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/persistentbottomsheet/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.persistentbottomsheet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnExpandSheet;
+    private LinearLayout layoutBottomSheet;
+    private BottomSheetBehavior bottomSheetBehavior;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnExpandSheet = findViewById(R.id.btn_expand_sheet);
+        layoutBottomSheet = findViewById(R.id.bottom_sheet_layout);
+
+        bottomSheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+
+        btnExpandSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+        });
+
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        btnExpandSheet.setText("Close Sheet");
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        btnExpandSheet.setText("Expand Sheet");
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+
+    }
+}
